c4ecd8bc2cc0019b93f5eefa85c2abd4d2cac12c
Russell Gordon <rgordon@lcs.on.ca>
2025-05-13 09:50:51 -0400

    Initial Commit

---
+++ SafeAreaExample/SafeAreaExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SafeAreaExampleApp.swift
+//  SafeAreaExample
+//
+//  Created by Russell Gordon on 2025-05-13.
+//
+
+import SwiftUI
+
+@main
+struct SafeAreaExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
